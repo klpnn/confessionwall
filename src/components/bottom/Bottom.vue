971c4e3f384f425ce1385c55b0176bd4574314a3
klpnn <915893924@qq.com>
2021-03-31 18:32:52 +0800

    基本架构

---
+++ src/components/bottom/Bottom.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="bottom">
+        冀ICP备19028957号-1
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Bottom"
+    }
+</script>
+
+<style scoped>
+    #bottom{
+        height: 4rem;
+        background-color: #c9c9c9;
+        font-size: 12px;
+        text-align: center;
+        line-height: 4rem;
+        color: white;
+    }
+</style>
---
+++ src/components/lostandfound/Lostandfound.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="landf">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Lostandfound"
+    }
+</script>
+
+<style scoped>
+
+</style>
